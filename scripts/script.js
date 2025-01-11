36b9a19c1aa36f7387f2d48086363f405e9842d2
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-01-11 22:38:51 +0400

    Validate order form inputs and clear fields on submission

--- scripts/script.js
+++ scripts/script.js
@@ -22,3 +22,27 @@ for (let i = 0; i < buttons.length; i++) {
     document.getElementById("order").scrollIntoView({behavior: "smooth"});
   }
 }
+
+// ORDER
+let burger = document.getElementById("burger");
+let name = document.getElementById("name");
+let phone = document.getElementById("phone");
+document.getElementById("order-action").onclick = function() {
+  let hasError = false;
+
+  [burger, name, phone].forEach(item => {
+    if (!item.value) {
+      item.parentElement.style.background = "red";
+      hasError = true;
+    } else {
+      item.parentElement.style.background = ""
+    }
+  });
+
+  if (!hasError) {
+    [burger, name, phone].forEach(item => {
+      item.value = ""; 
+    });
+    alert("Спасибо за заказ! Мы скоро свяжемся с вами");
+  }
+}
