729d7e46f36a894d9aa8910b1a16fc707bacbbb1
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-01-11 22:36:06 +0400

    Initialized smooth scrolling behavior to menu and header sections

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+// COVER
+document.getElementById("main-action-button").onclick = function () {
+  document.getElementById("menu").scrollIntoView({behavior: "smooth"});
+}
+
+document.getElementById("order-up").onclick = function () {
+  document.getElementById("header").scrollIntoView({behavior: "smooth"});
+}
