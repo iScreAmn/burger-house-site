61813151d022b7590c7ad8328807128a05a86ca0
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-01-11 22:36:54 +0400

    Enable smooth scrolling for navigation links

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,11 @@ document.getElementById("main-action-button").onclick = function () {
 document.getElementById("order-up").onclick = function () {
   document.getElementById("header").scrollIntoView({behavior: "smooth"});
 }
+
+// NAVIGATION
+let links = document.querySelectorAll(".nav__links > a");
+for (let i = 0; i < links.length; i++) {
+  links[i].onclick = function () {
+    document.getElementById(links[i].getAttribute("data-link")).scrollIntoView({behavior: "smooth"});
+  }
+}
