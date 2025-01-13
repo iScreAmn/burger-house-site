bc92f80cddb3b6327a6b2c3d0f9f8c42ba3edb96
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-01-13 17:21:00 +0400

    Add order notification pop-up

--- scripts/script.js
+++ scripts/script.js
@@ -43,9 +43,42 @@ document.getElementById("order-action").onclick = function() {
     [burger, name, phone].forEach(item => {
       item.value = ""; 
     });
-    alert("Спасибо за заказ! Мы скоро свяжемся с вами");
+    // alert("Спасибо за заказ! Мы скоро свяжемся с вами");
   }
 }
+
+// ORDER ALERT
+
+const button = document.querySelector(".order__button");
+const toast = document.querySelector(".toast");
+const closeIcon = document.querySelector(".close");
+const progress = document.querySelector(".progress");
+
+let timer1, timer2;
+
+button.addEventListener("click", () => {
+  toast.classList.add("active");
+  progress.classList.add("active");
+
+  timer1 = setTimeout(() => {
+    toast.classList.remove("active");
+  }, 5000); //1s = 1000 milliseconds
+
+  timer2 = setTimeout(() => {
+    progress.classList.remove("active");
+  }, 5300);
+});
+
+closeIcon.addEventListener("click", () => {
+  toast.classList.remove("active");
+
+  setTimeout(() => {
+    progress.classList.remove("active");
+  }, 300);
+
+  clearTimeout(timer1);
+  clearTimeout(timer2);
+});
 
 // ORDER FORM
 
